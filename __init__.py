3a1d1cad96d34314c8b5e48e03ff3fbb01d1a50f
Philip Deuchler <pbdeuchler@gmail.com>
2012-05-10 19:12:11 -0500

    Housekeeping

--- __init__.py
+++ __init__.py
@@ -6,13 +6,13 @@ classy
 ~~~~~~~~
 
 :copyright: (c) 2012 by Philip Deuchler.
-:license: ISC, see LICENSE for more details.
+:license: MIT, see LICENSE for more details.
 
 """
 
 __title__ = 'classy'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __build__ = 0x0000001
 __author__ = 'Philip Deuchler'
-__license__ = 'ISC'
+__license__ = 'MIT'
 __copyright__ = 'Copyright 2012 Philip Deuchler'
