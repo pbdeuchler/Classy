2ecd1c486bbc8f9a50fcaaa90cfad23ca1e3992d
Philip Deuchler <pbdeuchler@gmail.com>
2012-05-10 21:28:45 -0500

    Edited manifest... see if that works

--- Classy/__init__.py
+++ Classy/__init__.py
@@ -23,8 +23,8 @@ $$    $$/ $$ |$$    $$ |/     $$//     $$/ $$    $$ |
 """
 
 __title__ = 'Classy'
-__version__ = '0.0.8'
-__build__ = 0x0000007
+__version__ = '0.0.9'
+__build__ = 0x0000008
 __author__ = 'Philip Deuchler'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2012 Philip Deuchler'
