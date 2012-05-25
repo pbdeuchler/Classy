b4d494165d51ef8480d54c53365e6df94df5bfb1
Philip Deuchler <pbdeuchler@gmail.com>
2012-05-24 23:06:02 -0400

    Resetting branches in a really ghetto way

--- Classy/api.py
+++ Classy/api.py
@@ -2,31 +2,16 @@
 
 from __future__ import division
 from collections import Counter
-from multiprocessing import Pool
 import math
 
-STOP_WORDS = ['a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'was', 'were', 'will', 'with']
-
 class ClassifierNotTrainedException(Exception):
-	
+
 	def __str__(self):
 		return "Classifier is not trained."
-		
-class ClassifierNotAsyncException(Exception):
-
-	def __str__(self):
-		return "This classifier object is not async ready"
-		
-def unwrap_train(arg, **kwarg):
-	return Classy.train(*arg, **kwarg)
 
 class Classy(object):
-	
-	def __init__(self, async_process_count=False):
-		self.async = False
-		if async_process_count:
-			self.async = True
-			self.process_count = async_process_count
+
+	def __init__(self):
 		self.term_count_store = {}
 		self.data = {
 			'class_term_count': {},
@@ -35,15 +20,11 @@ class Classy(object):
 		}
 		self.total_term_count = 0
 		self.total_doc_count = 0
-		
-	def make_async(self, async_process_count=0):
-		self.async = True
-		self.process_count = async_process_count
-		
+
 	def train(self, document_source, class_id):
 		'''
 		Trains the classifier.
-		
+
 		'''
 		count = Counter(document_source)
 		try:
@@ -65,17 +46,12 @@ class Classy(object):
 			self.data['class_doc_count'][class_id] = 1
 		self.total_term_count += document_source.__len__()
 		self.total_doc_count += 1
+		self.compute_beta_priors()
 		return True
-		
-	def async_train(self, document_sources, class_id):
-		if not self.async: raise ClassifierNotAsyncException()
-		
-		
-		
+
 	def classify(self, document_input):
 		if not self.total_doc_count: raise ClassifierNotTrainedException()
-		
-		self.compute_beta_priors()
+
 		term_freq_matrix = Counter(document_input)
 		arg_max_matrix = []
 		for class_id in self.data['class_doc_count']:
@@ -91,21 +67,10 @@ class Classy(object):
 			arg_max_matrix.insert(0, (class_id, arg_max))
 		arg_max_matrix.sort(key=lambda x:x[1])
 		return (arg_max_matrix[-1][0], arg_max_matrix[-1][1])
-		
+
 	def compute_beta_priors(self):
 		if not self.total_doc_count: raise ClassifierNotTrainedException()
-		
+
 		for class_id in self.data['class_doc_count']:
 			tmp = self.data['class_doc_count'][class_id] / self.total_doc_count
 			self.data['beta_priors'][class_id] = math.log(tmp)
-			
-	def clean_text(self, text_input, parse_stop_words=False, stop_list=STOP_WORDS):
-		term_vector = []
-		if parse_stop_words:
-			for term in text_input.split():
-				term_vector.insert(0, term)
-			return term_vector
-		for term in text_input.split():
-			if term not in stop_list:
-				term_vector.insert(0, term)
-		return term_vector	
